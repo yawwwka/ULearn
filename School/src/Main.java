75c3b2c9696f2e6b18b758d87f50a659c5e5801d
Dimas <109547506+yawwwka@users.noreply.github.com>
2023-04-15 00:57:21 +0500

    Delete Main.java

--- School/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Teacher t = new Teacher("Dimas", "Pol", "Inf", 12);
-        Teacher t1 = new Teacher("Dim", "P", "I", 15);
-        System.out.println(t);
-        System.out.println(t1);
-    }
-}
-

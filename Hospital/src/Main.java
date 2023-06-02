e581720009f0431580ef6d07ab78e013cf3a1165
Dmitry <polds801@mail.ru>
2023-06-02 22:27:31 +0500

    s

--- Hospital/src/Main.java
+++ Hospital/src/Main.java
@@ -1,69 +1,103 @@
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO напишите консольное приложение для работы с классом больница, учитывая, что сгенерированные и вычисленные переменные,
-        // нельзя будет менять.
-        Hospital hosp = new Hospital(5);
-        System.out.println(hosp.patientsTemperatures);
+        Hospital hosp = new Hospital(13);
+        System.out.println(hosp.getHealthyCount());
+        System.out.println(hosp.getAvgTemp());
+        System.out.println(hosp.getTempString());
+        System.out.println(hosp.getReport());
     }
 }
 
 class Hospital {
-    int patientsCount, countHealthy;
-    float[] patientsTemperatures = generatePatientsTemperatures();
+    private final int patientsCount;
+    private final int healthyCount;
+    private final double avgTemp;
+    private final float[] temperatures;
+    private final String tempString;
+    private boolean flag = false;
+
+    public int getHealthyCount() {
+        return healthyCount;
+    }
+
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public String getTempString() {
+        return tempString;
+    }
 
     public Hospital(int patientsCount) {
-        try {
-            this.patientsCount = patientsCount;
-        } catch (IllegalArgumentException e) {
-            throw e;
-        }
+        this.patientsCount = patientsCount;
+        this.temperatures = generatePatientsTemperatures();
+        this.healthyCount = getCountHealthy();
+        this.avgTemp = getAverageTemp();
+        this.tempString = getTemperaturesToString();
     }
 
     //нужно кол-во пациентов
-    public float[] generatePatientsTemperatures() {
-        float[] temps = new float[patientsCount];
-        for (int i = 0; i < patientsCount; i++){
-            temps[i] = (float) (Math.ceil((32 + (Math.random() * 40)) * 100) / 100);
-        }
-        sout
-        return temps;
+    float[] generatePatientsTemperatures() {
+        if (!flag) {
+            try {
+                final int roundCount = 10;
+                float[] temperatures = new float[patientsCount];
+                for (int i = 0; i < patientsCount; i++) {
+                    float temperature = (Math.round(((new Random()).nextInt(8) + 32 + (new Random()).nextFloat()) * roundCount));
+                    temperatures[i] = temperature / roundCount;
+                }
+                flag = true;
+                return temperatures;
+            } catch (Exception e) {
+                throw new IllegalArgumentException();
+            }
+        } else return temperatures;
     }
 
-    //нужна генерация температур
-//    public int getCountHealthy() {
-//        //TODO реализовать подсчёт кол-ва здоровых(36,2 < x < 36,9) в сгенерированном массиве температур
-//        // если метод уже был вызван ранее, не выполнять подсчет снова, а возращать текущее вычисленное значение
-//        // если генерация температур не была выполнена, вызвать метод генерации
-//
-//    }
-//
-//    //нужна генерация температур
-//    public String getTemperaturesToString() {
-//        //TODO реализовать получение массива температур в виде строки <double>, <double>, <double>
-//        // если метод уже был вызван ранее, не выполнять подсчет снова, а возращать текущее вычисленное значение
-//        // если генерация температур не была выполнена, вызвать метод генерации
-//    }
-//
-//    //Нужна генерация температур
-//    public double getAverageTemp() {
-//        //TODO реализовать получение средней температуры из массива
-//        // если метод уже был вызван ранее, не выполнять подсчет снова, а возращать текущее вычисленное значение
-//        // если генерация температур не была выполнена, вызвать метод генерации
-//    }
-//
-//    //нужны температуры, средняя температура и кол-во здоровых
-//    public static String getReport(float[] temperatureData) {
-//        //TODO реализовать получение отчёта в формате приложенном ниже
-//        // если метод уже был вызван ранее, не выполнять подсчет снова, а возращать текущее вычисленное значение
-//        // если все необходимые переменные не были получены ранее, вызвать все методы необходимые для получения отчёта
-//        /*TODO Формат вывода:
-//           Температуры пациентов: 37,5 36,9 38,2 33,5 32,2
-//           Средняя температура: 35,67
-//           Количество здоровых: 1
-//         */
-//        return "";
-//    }
+     double getAverageTemp() {
+        double summ = 0, avg;
+        for (float each : temperatures) {
+            summ += each;
+        }
+        avg = summ / temperatures.length;
+        return avg;
+    }
+
+    int getCountHealthy() {
+        int healthyCount = 0;
+        for (float patient : temperatures) {
+            if (36.2 < patient && patient < 36.9) {
+                healthyCount++;
+            }
+        }
+        return healthyCount;
+    }
+
+    String getTemperaturesToString() {
+        String tempString = "";
+        for (int i = 0; i < temperatures.length; i++) {
+            if (i != temperatures.length - 1) {
+                tempString += String.valueOf(temperatures[i]).replace('.', ',') + " ";
+            } else {
+                tempString += String.valueOf(temperatures[i]).replace('.', ',');
+            }
+        }
+        return tempString;
+    }
+
+    //нужны температуры, средняя температура и кол-во здоровых
+    public String getReport() {
+        //TODO реализовать получение отчёта в формате приложенном ниже
+        // если метод уже был вызван ранее, не выполнять подсчет снова, а возращать текущее вычисленное значение
+        // если все необходимые переменные не были получены ранее, вызвать все методы необходимые для получения отчёта
+        /*TODO Формат вывода:
+           Температуры пациентов: 37,5 36,9 38,2 33,5 32,2
+           Средняя температура: 35,67
+           Количество здоровых: 1
+         */
+        return "Температуры пациентов: " + tempString + "\nСредняя температура: " + String.format("%.2f", avgTemp).replace('.', ',') + "\nКоличество здоровых: " + healthyCount;
+    }
 }
